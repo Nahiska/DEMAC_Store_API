6c43757717c71a53bf11ec4c186415b38da92bac
MelinaLabra <melina.lb04@gmail.com>
2023-05-24 22:42:46 -0300

    added productById API

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -15,7 +15,21 @@ const getProducts = async () => {
         throw new Error("Error while fetching products");
     }
 };
-
+const getProductById = async (productId) => {
+    try {
+          const product = await Product.findByPk(productId, {
+            include: [
+                { association: "brands" },
+                { association: "subcategories", include: [{ association: "categories"}] },
+            ],
+          });
+          return product;
+    } catch (error) {
+          console.error("Error while fetching product: ", error);
+          throw new Error("Error ehile fetching product: ");
+    }
+};
 module.exports = {
-    getProducts
+    getProducts,
+    getProductById
 }
