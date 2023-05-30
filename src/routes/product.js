e83ba54f170ce3ce528960c20067a4b2d810efbe
camila-yniguez <cami.yniguez@gmail.com>
2023-05-30 00:13:25 -0300

    Change API routes

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { getProducts, getProductById, createProduct, updateProduct, deleteProduct } = require("../controllers/product.controller");
-const verifyToken = require("../middlewares/jwt.middleware");
+// const verifyToken = require("../middlewares/jwt.middleware");
 const validate = require("../validations/index.validator");
 const { productValidationRules } = require("../validations/postProduct.validator");
 
 router
-    .get("/", verifyToken, getProducts)
+    .get("/", getProducts)
     .get("/:id", getProductById)
     .post("/create", productValidationRules(), validate, createProduct)
     .put("/:id", productValidationRules(), validate, updateProduct)
